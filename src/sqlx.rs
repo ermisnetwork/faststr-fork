bb3fb4d28dca91b9f3fcd2a1215b2756fdd57a29
linh <linh@belo.im>
2024-07-02 16:00:38 +0700

    implemented type

--- src/sqlx.rs
+++ src/sqlx.rs
@@ -1,4 +1,4 @@
-use sqlx::{database::HasValueRef, Database, Decode, Encode};
+use sqlx::{database::{HasArguments, HasValueRef}, encode::IsNull, Database, Decode, Encode, Type, TypeInfo};
 
 impl<'r, DB: Database> Decode<'r, DB> for crate::FastStr where String: Decode<'r, DB>{
     fn decode(value: <DB as HasValueRef<'r>>::ValueRef) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
@@ -7,8 +7,14 @@ impl<'r, DB: Database> Decode<'r, DB> for crate::FastStr where String: Decode<'r
     }
 }
 
-impl<DB: Database> Encode<DB> for crate::FastStr where String: Encode<DB>{
-    fn encode_by_ref(&self) -> <DB as HasValueRef<'_>>::ValueRef {
-        self.as_str().encode_by_ref()
+impl<'q, DB: Database> Encode<'q, DB> for crate::FastStr where String: Encode<'q, DB>{
+    fn encode_by_ref(&self, buf: &mut <DB as HasArguments<'q>>::ArgumentBuffer) -> IsNull {
+        <String as Encode<DB>>::encode_by_ref(&self.to_string(), buf)
+    }
+}
+
+impl<DB: Database> Type<DB> for crate::FastStr where String: Type<DB> {
+    fn type_info() -> <DB as Database>::TypeInfo {
+        <String as Type<DB>>::type_info()
     }
 }
