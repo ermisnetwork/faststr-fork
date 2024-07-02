a41a147070c54075c5525ab5efa68a983790b3ed
linh <linh@belo.im>
2024-07-02 15:27:15 +0700

    implemented encode

--- src/sqlx.rs
+++ src/sqlx.rs
@@ -1,13 +1,14 @@
-use sqlx::{database::HasValueRef, Database, Decode};
-// impl<R> FromRow<'_, R> for crate::FastStr where R: Row {
-//     fn from_row(row: &R) -> sqlx::Result<Self> {
-//         let s: String = FromRow::from_row(row)?;
-//     }
-// }
+use sqlx::{database::HasValueRef, Database, Decode, Encode};
 
 impl<'r, DB: Database> Decode<'r, DB> for crate::FastStr where String: Decode<'r, DB>{
     fn decode(value: <DB as HasValueRef<'r>>::ValueRef) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
         let value = <String as Decode<DB>>::decode(value)?;
         Ok(crate::FastStr::from(value))
+    }
+}
+
+impl<DB: Database> Encode<DB> for crate::FastStr where String: Encode<DB>{
+    fn encode_by_ref(&self) -> <DB as HasValueRef<'_>>::ValueRef {
+        self.as_str().encode_by_ref()
     }
 }
